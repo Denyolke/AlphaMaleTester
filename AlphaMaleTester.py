716bc8f8bc75d68ed2f522d5e5e710f71487212c
Denyolke <tatyidani@gmail.com>
2024-05-12 13:00:12 +0200

    v2

--- AlphaMaleTester.py
+++ AlphaMaleTester.py
@@ -1,18 +1,19 @@
 import webbrowser
-
+print("Tell me your name: ")
 def getMogged():
     while(1):
-        print(" kys")
+        print("kys")
+        
         
 try:
     a = str(input()).lower()
-    lista = ["fanti","oli","fantom","fanthomas77","olikavezer","olcsi","sebi"]
+    lista = ["fanti","oli","fantom","fanthomas77","olikavezer","olcsi","sebi","darksnhun"]
     
     if a.isalpha() == False or a in lista:
         getMogged()
     elif a.isalpha():
         webbrowser.open("https://youtu.be/0GVExpdmoDs?t=183")
-        print("yes")
+        print("Based")
 
 
 except Exception as e:
